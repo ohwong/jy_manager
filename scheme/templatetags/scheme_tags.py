52ca2a260e7b037c72bf24ce9d1c215efcbb4e1c
wangjiaxi <wangjiaxi@cnicg.cn>
2017-08-27 16:14:12 +0800

    fix attribute error

--- scheme/templatetags/scheme_tags.py
+++ scheme/templatetags/scheme_tags.py
@@ -9,5 +9,7 @@ register = Library()
 @register.simple_tag
 def current_scheme():
     """Converts a string into all lowercase"""
-
-    return MissionImage.objects.last().image.url
+    try:
+        return MissionImage.objects.last().image.url
+    except AttributeError:
+        return ""

be2cd436a8dd4333c7c2550e932ac53116f53183
wangjiaxi <wangjiaxi@cnicg.cn>
2018-06-01 22:09:06 +0800

    fix admin

--- aircraft/admin.py
+++ aircraft/admin.py
@@ -10,3 +10,9 @@ class AircraftAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def get_actions(self, request):
+        actions = super(AircraftAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions

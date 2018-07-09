72464ad160a2693ed9cb916668679ba421988f0a
wangjiaxi <wangjiaxi@cnicg.cn>
2018-07-09 18:43:26 +0800

    fix

--- aircraft/models.py
+++ aircraft/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class AircraftManager(models.Manager):
     def valid_name_objects(self):
-        return self.filter(aircraft_code__regex='^([a-zA-Z]{1,2}-[0-9]{4,5})|N/A$')
+        return self.filter(aircraft_code__regex='^([a-zA-Z]{1,2}-[0-9]{4,5})|(N/A)$')
 
 
 class Aircraft(models.Model):
